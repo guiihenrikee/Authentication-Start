eea2407e4478619745798636afbedd8d78b15f03
Guilherme <gui.ghrp@gmail.com>
2022-06-23 12:01:30 -0300

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,78 @@
+
+require("dotenv").config()
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const app = express();
+const encrypt = require('mongoose-encryption');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+//---------DB-------------------------
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
+
+const User = new mongoose.model("User", userSchema)
+//----------------------------------
+
+//-----------ROUTES-----------------------
+app.get("/", (req, res) => {
+  res.render("Home")
+})
+
+app.get("/login", (req, res) => {
+  res.render("Login")
+})
+
+app.get("/register", (req, res) => {
+  res.render("Register")
+})
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  newUser.save((err) => {
+    res.render("secrets") ? !err : res.send(err)
+  })
+})
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, (err, foundUser) => {
+    if (err) {
+        console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets")
+        }
+      }
+    }
+  })
+})
+//-----------------------------------
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
